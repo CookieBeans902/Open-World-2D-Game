ed07872fd56fbb79afe4d7d6ea02a0e7ac5cbb66
anshu <anshukumr2006@gmail.com>
2025-06-15 19:58:39 +0530

    added move types

---
+++ Assets/Script/Movement/MTypes.cs
@@ -0,0 +1,15 @@
+public enum MoveType {
+    Random,
+    Chase,
+    Patrol,
+    RunAway,
+    Follow,
+}
+
+public enum MoveState {
+    Random,
+    Chasing,
+    Patrolling,
+    RunningAway,
+    Following,
+}
